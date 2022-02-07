9ba98b23413f6ffc47c39ff29f980c45a5c10f2b
Kevin <kevincespedes2004@gmail.com>
2022-02-07 16:56:56 -0500

    update PizarraJs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const dif = canvas.getBoundingClientRect();
 let painting, color, lineWidth, difX, difY;
+const btnClear = document.getElementById("btn__limpiar");
 canvas.addEventListener("mousedown", (e) => {
   difX = e.clientX - dif.left;
   difY = e.clientY - dif.top;
@@ -10,7 +11,6 @@ canvas.addEventListener("mousedown", (e) => {
   lineWidth = document.getElementById("range").value;
   ctx.beginPath();
 });
-
 canvas.addEventListener("mousemove", (e) => {
   if (painting) {
     dibujar(difX, difY, e.clientX - dif.left, e.clientY - dif.top);
@@ -22,11 +22,16 @@ canvas.addEventListener("mouseup", (e) => {
   painting = false;
   ctx.closePath();
 });
+// Clear Canvas
+btnClear.addEventListener("click", (e) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 function dibujar(x1, y1, x2, y2) {
   ctx.strokeStyle = color;
   ctx.lineWidth = lineWidth;
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
+  ctx.lineCap = "round";
   ctx.stroke();
 }
